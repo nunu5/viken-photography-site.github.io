1211ecf5ce2c24000d056ff934027326fb1a3008
Shraddha <codeinshraddha7@gmail.com>
2021-10-31 11:14:51 +0530

    photography

---
+++ grid.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $(window).bind('scroll', function() {
+    var navHeight = $( window ).height() - 70;
+          if ($(window).scrollTop() > navHeight) {
+              $('nav').addClass('fixed');
+          }
+          else {
+              $('nav').removeClass('fixed');
+          }
+     });
+ });
+
+
+
+
+
+
+
+
